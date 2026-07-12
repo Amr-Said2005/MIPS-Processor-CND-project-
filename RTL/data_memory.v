70f1c1b938dad2491f3bf020bd924f8e73ac31cc
iahmady <i.ahmady@aucegypt.edu>
2026-07-12 14:42:13 +0300

    added read and write logic

--- RTL/data_memory.v
+++ RTL/data_memory.v
@@ -1,7 +1,21 @@
 module data_memory (
-    input wire  [31:0] address, 
+    input wire [31:0] address, 
     input wire        memRead,
     input wire        memWrite,
     input wire        clk,
+    input wire [31:0] data_in,
     output reg [31:0] data_out
 );
+
+reg [1023:0] mem [31:0];
+
+always @(clk) begin
+
+    if(memRead) begin
+        data_out <= mem[address];
+    end
+    else if(memWrite) begin
+        mem[address] <= data_in;
+    end
+end
+endmodule 
