48b33e471854d6e4906bd9b3b8ca9abd663a6c56
Abdel Rahman El Helw <abdelrahmanelhelw@aucegypt.edu>
2026-07-22 13:39:53 +0300

    g

--- RTL/iexecute.v
+++ RTL/iexecute.v
@@ -31,30 +31,18 @@ module iexecute(
     output [31:0] jump_target,
     output [4:0] write_reg,
     output zero,
+    output [31:0] store_data,
     output PCSrc
 );
 
     wire [31:0] alu_b;
-    wire [31:0] direct_target;
-    wire [31:0] taken_target;
-    wire [31:0] mem_data;         // data-memory read (declared here: used below)
     assign jump_target ={pc_plus4[31:28], jump_index, 2'b00};
     wire [31:0] branch_offset;
     assign branch_offset = sign_ext_imm << 2;
     assign branch_target = pc_plus4 + branch_offset;
-    Mux2to1 JumpTargetMux (
-        .A   (branch_target),
-        .B   (jump_target),
-        .Sel (Jump),
-        .Y   (direct_target)
-    );
+    assign PCSrc = (Branch & zero) | Jump;
+    assign store_data = read_data_2;
 
-    Mux2to1 MemTargetMux (
-        .A   (direct_target),
-        .B   (mem_data),          // jmn: Memory[R[rs]+imm]   pmc: Memory[R[rt]]
-        .Sel (JMN | pmc),
-        .Y   (taken_target)
-    );
 
 
     // 3-way destination select: rt (I-type) / rd (R-type) / rs (swi post-increment)
