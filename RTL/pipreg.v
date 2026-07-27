cbf5aabfeae45079d6df60123e709a783e3ce641
simplescholar1 <mohamedytaha@aucegypt.edu>
2026-07-27 11:10:46 +0300

    Update pipreg.v

--- RTL/pipreg.v
+++ RTL/pipreg.v
@@ -1,21 +1,17 @@
-module #(prameter N) pipreg(
-input hold,
-input clear,
-input clk,
-input  [n-1:0] in;
-output reg [n-1:0] out;
+module pipreg #(parameter N = 32) (
+    input hold,
+    input clear,
+    input clk,
+    input  [N-1:0] in,
+    output reg [N-1:0] out
+);
 
-
-always@(posedge clk) begin
-if(clear)
-out<={N{1'b0}};
-
-else if(hold) 
-
-out<=out;
-
-else
-out<=in;
+always @(posedge clk) begin
+    if (clear)
+        out <= {N{1'b0}};
+    else if (hold) 
+        out <= out; 
+    else
+        out <= in;  
 end
-endmodule 
-
+endmodule
